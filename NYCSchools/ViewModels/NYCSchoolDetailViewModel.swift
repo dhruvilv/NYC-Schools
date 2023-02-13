96bffe21be98e6a52c0439a7387475bd64a0d597
Dhruvil Vyas <dhruvil@saavn.com>
2023-02-12 20:14:33 -0800

    Added a comment

--- NYCSchools/ViewModels/NYCSchoolDetailViewModel.swift
+++ NYCSchools/ViewModels/NYCSchoolDetailViewModel.swift
@@ -18,6 +18,12 @@ class NYCSchoolDetailViewModel {
   init(schoolInfo: NYCSchoolInfo, apiService: NYCSchoolAPIService) {
     self.schoolInfo = schoolInfo    
     self.apiService = apiService
+    
+    
+    /// NOTE: I don't like the fact that we cannot fetch the SAT score based off a school `databaseNumber`
+    /// which would fetch us a single object since we already have the databaseNumber at hand. There might
+    /// be such an API, but atleast I was not able to find it. Nevertheless, we fetch SAT scores for all
+    /// the schools and filter out the respones.
     apiService.fetchSATScores { [weak self] result in
       switch result {
       case .success(let scoreList):
