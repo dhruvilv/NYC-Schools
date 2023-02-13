bed67b7f60992d9ac4732961d0ed1d162451ad43
Dhruvil Vyas <dhruvil@saavn.com>
2023-02-12 19:10:07 -0800

    Refactor `APIService` protocol

--- NYCSchools/API/APIService.swift
+++ NYCSchools/API/APIService.swift
@@ -25,6 +25,64 @@ typealias ResultCompletion<T: Decodable> = (Result<T, APIError>) -> Void
 // data fetching into 2 parts.
 
 protocol APIService {
+  var session: URLSession { get }
   func performFetch(_ request: URLRequest, _ completion: @escaping DataCompletion)
   func parseDecodable<T: Decodable>(result: DataResult, completion: @escaping ResultCompletion<T>)
+}
+
+extension APIService {
+  
+  func performFetch(_ request: URLRequest, _ completion: @escaping (Result<Data, APIError>) -> Void) {
+    let task = session.dataTask(with: request) { (data, response, error) in
+      if let error = error {
+        completion(.failure(.networkingError(error)))
+      }
+      guard let http = response as? HTTPURLResponse, let data = data else {
+        completion(.failure(.invalidError))
+        return
+      }
+      
+      switch http.statusCode {
+      
+      case 200:
+        completion(.success(data))
+        print("success")
+        
+      case 400...499:
+        let body = String(data: data, encoding: .utf8)
+        completion(.failure(.requestError(http.statusCode, body ?? "<no body>")))
+        print("failure")
+        
+      case 500...599:
+        completion(.failure(.serverError))
+        
+      default:
+        fatalError("Unhandled HTTP Status Code: \(http.statusCode)")
+      }
+    }
+    task.resume()
+  }
+  
+  func parseDecodable<T: Decodable>(result: Result<Data, APIError>, completion: @escaping ResultCompletion<T>) {
+    switch result {
+    case .success(let data):
+      // Successfully fetched Data. Parse using JSONDecoder
+      do {
+        let responseObject = try JSONDecoder().decode(T.self, from: data)
+        DispatchQueue.main.async {
+          completion(.success(responseObject))
+        }
+      } catch {
+        // Received Decoding Error. Deliver error completion
+        guard let decodingError = error as? DecodingError else { return }
+        DispatchQueue.main.async {
+          completion(.failure(.decodingError(decodingError)))
+        }
+      }
+    case .failure(let error):
+      DispatchQueue.main.async {
+        completion(.failure(error))
+      }
+    }
+  }
 }
--- NYCSchools/API/NYCSchoolAPIService.swift
+++ NYCSchools/API/NYCSchoolAPIService.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct NYCSchoolAPIService {
+struct NYCSchoolAPIService: APIService {
   
   private static let SchoolListJSON = "s3k6-pzi2.json"
   private static let SchoolDetailJSON = "f9bf-2cp4.json"
 
-  let session: URLSession
+  var session: URLSession
   private let baseURL = "https://data.cityofnewyork.us/resource"
   
   init(session: URLSession = .shared) {
@@ -36,64 +36,7 @@ struct NYCSchoolAPIService {
       parseDecodable(result: result, completion: completion)
     }
   }
-}
-
-extension NYCSchoolAPIService: APIService {
   
-  func performFetch(_ request: URLRequest, _ completion: @escaping (Result<Data, APIError>) -> Void) {
-    let task = session.dataTask(with: request) { (data, response, error) in
-      if let error = error {
-        completion(.failure(.networkingError(error)))
-      }
-      guard let http = response as? HTTPURLResponse, let data = data else {
-        completion(.failure(.invalidError))
-        return
-      }
-      
-      switch http.statusCode {
-      
-      case 200:
-        completion(.success(data))
-        print("success")
-        
-      case 400...499:
-        let body = String(data: data, encoding: .utf8)
-        completion(.failure(.requestError(http.statusCode, body ?? "<no body>")))
-        print("failure")
-        
-      case 500...599:
-        completion(.failure(.serverError))
-        
-      default:
-        fatalError("Unhandled HTTP Status Code: \(http.statusCode)")
-      }
-    }
-    task.resume()
-  }
-  
-  func parseDecodable<T: Decodable>(result: Result<Data, APIError>, completion: @escaping ResultCompletion<T>) {
-    switch result {
-    case .success(let data):
-      // Successfully fetched Data. Parse using JSONDecoder
-      do {
-        let responseObject = try JSONDecoder().decode(T.self, from: data)
-        DispatchQueue.main.async {
-          completion(.success(responseObject))
-        }
-      } catch {
-        // Received Decoding Error. Deliver error completion
-        guard let decodingError = error as? DecodingError else { return }
-        DispatchQueue.main.async {
-          completion(.failure(.decodingError(decodingError)))
-        }
-      }
-    case .failure(let error):
-      DispatchQueue.main.async {
-        completion(.failure(error))
-      }
-    }
-  }
-
   struct NYCSchoolListAPIResponse: Decodable {
     let databaseNumber: String
     let schoolName: String
